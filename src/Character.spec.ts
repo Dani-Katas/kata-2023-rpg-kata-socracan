580691f354f85c1e694011e7db06629491f23bd3
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-13 21:54:47 +0100

    level one and restore HP test

--- src/Character.spec.ts
+++ src/Character.spec.ts
@@ -2,9 +2,23 @@ import { describe, expect, it } from "vitest";
 import { Character } from "./Character.js";
 
 describe("Character", () => {
+  it("starts with full hp", () => {
+    const character = Character.spawn();
+
+    const initialHealth = 1000;
+    expect(character.hasHealth(initialHealth)).toBe(true)
+  });
+
   it("is alive when created", () => {
-    const character = new Character();
+    const character = Character.spawn();
 
     expect(character.isAlive()).toBe(true)
+  });
+
+  it("starts at level one", () => {
+    const character = Character.spawn();
+
+    const initialLevel = 1;
+    expect(character.isLevel(initialLevel)).toBe(true)
   });
 });
--- src/Character.ts
+++ src/Character.ts
@@ -1,5 +1,18 @@
 export class Character {
+
+  static spawn() {
+    return new Character()
+  }
+
   isAlive() {
     return true;
+  }
+
+  hasHealth(initialHealth: number) {
+    return true
+  }
+
+  isLevel(initialLevel: number) {
+    return true
   }
 }
