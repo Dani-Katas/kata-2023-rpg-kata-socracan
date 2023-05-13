70bed7be849495fa70f28f17e40d0b56fa4a016e
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-13 22:10:47 +0100

    implement dealDamage method

--- src/Character.spec.ts
+++ src/Character.spec.ts
@@ -1,6 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { Character } from "./Character.js";
 
+/**
+ * 1. All Characters, when created, have:
+ *     - Health, starting at 1000
+ *     - Level, starting at 1
+ *     - May be Alive or Dead, starting Alive (Alive may be a true/false)
+ *
+ * 1. Characters can Deal Damage to Characters.
+ *     - Damage is subtracted from Health
+ *     - When damage received exceeds current Health, Health becomes 0 and the character dies
+ *
+ * 1. A Character can Heal a Character.
+ *     - Dead characters cannot be healed
+ *     - Healing cannot raise health above 1000
+ */
+
 describe("Character", () => {
   it("starts with full hp", () => {
     const character = Character.spawn();
@@ -21,4 +36,24 @@ describe("Character", () => {
     const initialLevel = 1;
     expect(character.isLevel(initialLevel)).toBe(true)
   });
+
+  it("recieves damage", () => {
+    const character = Character.spawn();
+    const attacker = Character.spawn();
+
+    const damageDealt = 200;
+    attacker.dealDamage(character, damageDealt)
+
+    expect(character.hasHealth(800)).toBe(true)
+  });
+
+  // it("deals damage to another character", () => {
+  //   const character = Character.spawn();
+  //   const attacker = Character.spawn();
+  //
+  //   attacker.attack(character, 1000)
+  //
+  //   expect(character.hasHealth(0)).toBe(true)
+  //   expect(character.isDead()).toBe(true)
+  // });
 });
--- src/Character.ts
+++ src/Character.ts
@@ -1,4 +1,5 @@
 export class Character {
+  private health = 1000;
 
   static spawn() {
     return new Character()
@@ -8,11 +9,15 @@ export class Character {
     return true;
   }
 
-  hasHealth(initialHealth: number) {
-    return true
+  hasHealth(health: number) {
+    return this.health == health
   }
 
   isLevel(initialLevel: number) {
     return true
+  }
+
+  dealDamage(character: Character, damageDealt: number) {
+    character.health = character.health - damageDealt
   }
 }
