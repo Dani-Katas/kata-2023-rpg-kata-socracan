eeab549ac617a5da614459e8487bf4e72c310b28
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-13 22:30:43 +0100

    refactor tests

--- src/Character.spec.ts
+++ src/Character.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest";
-import { Character } from "./Character.js";
+import { describe, expect, it } from "vitest"
+import { Character } from "./Character.js"
 
 /**
  * 1. All Characters, when created, have:
@@ -17,51 +17,55 @@ import { Character } from "./Character.js";
  */
 
 describe("Character", () => {
-  it("starts with full hp", () => {
-    const character = Character.spawn();
+  describe("spawning", () => {
+    it("starts with full hp", () => {
+      const character = Character.spawn()
 
-    const initialHealth = 1000;
-    expect(character.hasHealth(initialHealth)).toBe(true)
-  });
+      const initialHealth = 1000
+      expect(character.hasHealth(initialHealth)).toBe(true)
+    })
 
-  it("is alive when created", () => {
-    const character = Character.spawn();
+    it("is alive when created", () => {
+      const character = Character.spawn()
 
-    expect(character.isAlive()).toBe(true)
-  });
+      expect(character.isAlive()).toBe(true)
+    })
 
-  it("starts at level one", () => {
-    const character = Character.spawn();
+    it("starts at level one", () => {
+      const character = Character.spawn()
 
-    const initialLevel = 1;
-    expect(character.isLevel(initialLevel)).toBe(true)
-  });
+      const initialLevel = 1
+      expect(character.isLevel(initialLevel)).toBe(true)
+    })
+  })
 
-  it("deals damage to other character", () => {
-    const character = Character.spawn();
-    const attacker = Character.spawn();
+  describe("dealing damage", () => {
+    it("deals damage to other character", () => {
+      const character = Character.spawn()
+      const attacker = Character.spawn()
 
-    const damageDealt = 200;
-    attacker.dealDamage(character, damageDealt)
+      const damageDealt = 200
+      attacker.dealDamage(character, damageDealt)
 
-    expect(character.hasHealth(800)).toBe(true)
-  });
+      expect(character.hasHealth(800)).toBe(true)
+    })
 
-  it("is dead when receiving enough damage", () => {
-    const character = Character.spawn();
-    const attacker = Character.spawn();
+    it("is dead when receiving enough damage", () => {
+      const character = Character.spawn()
+      const attacker = Character.spawn()
 
-    attacker.dealDamage(character, 1000)
+      attacker.dealDamage(character, 1000)
 
-    expect(character.isDead()).toBe(true)
-  });
+      expect(character.isDead()).toBe(true)
+    })
 
-  it("never has health below zero", () => {
-    const character = Character.spawn();
-    const attacker = Character.spawn();
+    it("never has health below zero", () => {
+      const character = Character.spawn()
+      const attacker = Character.spawn()
 
-    attacker.dealDamage(character, 1001)
+      attacker.dealDamage(character, 1001)
 
-    expect(character.hasHealth(0)).toBe(true)
-  });
-});
+      expect(character.hasHealth(0)).toBe(true)
+    })
+  })
+})
